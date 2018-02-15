1f3de4d0cb9c8526f3c70f371869eed682018f66
Kevin Saavedra <kevin.saavedra@oregonmetro.gov>
2018-02-15 09:07:53 -0800

    Add jupyter notebook.

--- phed_calc.py
+++ phed_calc.py
@@ -3,10 +3,15 @@ import pandas as pd
 #import numpy as np
 
 def main():
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Feb2017_test/Feb2017_test.csv'))
-    df_meta = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Feb2017_test/TMC_Identification.csv'))
+    df = pd.read_csv(os.path.join(os.path.dirname('__file__'), 'Feb2017_test/Feb2017_test.csv')) #fix in script implementation
+    df_meta = pd.read_csv(os.path.join(os.path.dirname('__file__'), 'Feb2017_test/TMC_Identification.csv'), 
+                          usecols=['tmc', 'road', 'direction', 'intersection', 'state', 'county', 'miles',
+                                                  'tmclinear', 'aadt', 'aadt_singl', 'aadt_combi' ])
+    df_odot = pd.read_csv(os.path.join(os.path.dirname('__file__'), 'Feb2017_test/odot_edt.csv'))
     df = pd.merge(df, df_meta, left_on=df['tmc_code'], right_on=['tmc'], how='inner')
-    print (df)
+
+    # Return a selected row for test purposes only
+    print(df.loc[df['tmc_code'] == '114-04368'])
 
 if __name__ == '__main__':
     main()

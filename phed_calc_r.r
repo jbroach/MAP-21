ad53aedcf893b58d3b4804f75f203a9f8ed6fa99
saashimi <saavedra.kevin@gmail.com>
2018-02-27 08:04:05 -0800

    Join peak hour factor and main datatables.

--- phed_calc_r.r
+++ phed_calc_r.r
@@ -1,13 +1,15 @@
 # Use data.table library
-# library(data.table)
+library(data.table)
 
-tb_speed <- read.csv("C:/Users/saavedrak/metro_work/PHED/Feb2017_test/Feb2017_test.csv")
-# Format as DateTime equivalent:
-tb_speed$pk_hr <- as.integer(strftime(tb_speed$measurement_tstamp, format="%H"))
+tb_spd <- fread("C:/Users/saavedrak/metro_work/PHED/Feb2017_test/Feb2017_test.csv")
+# Extract hour from datetime timestamp:
+tb_spd[, pk_hr := as.integer(strftime(measurement_tstamp, format="%H"))]
+tb_peak <- fread("H:/map21/perfMeasures/phed/data/peakingFactors_join_edit.csv")
+# Convert text time to standalone integer hour. 
+tb_peak[, pk_hour := as.integer(strftime(strptime(startTime, format="%H:%M"), format="%H"))]
+tb <- merge(x=tb_spd, y=tb_peak, by.x="pk_hr", by.y="pk_hour")
 
-#tb_peak <- fread("H:/map21/perfMeasures/phed/data/urban_tmc.csv")
 #tb_meta <- fread("H:/map21/perfMeasures/phed/data/TMC_Identification_NPMRDS (Trucks and passenger vehicles).csv")
 #tb_here <- fread("H:/map21/perfMeasures/phed/data/HERE_OR_Static_TriCounty_edit.csv")
-#merge(x=tb_speed, y=tb_peak, by.x="tmc_code", by.y=
 
 #source("C:/Users/saavedrak/metro_work/PHED/phed_calc_r.r")

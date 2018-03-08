2603a5d59e05aa65f561c81f7abee409935d6b09
saashimi <saavedra.kevin@gmail.com>
2018-03-08 14:58:34 -0800

    edit R file for more style-compliant variable names.

--- phed_calc_r.r
+++ phed_calc_r.r
@@ -2,68 +2,68 @@
 library(data.table)
 library(lubridate) # Speed up slow time parsing behavior.
 
-per_capita_TED <- function(sum_11_mo) {
-  year_adjusted_TED <- (sum_11_mo / 11) + sum_11_mo
-  pop_PDX <- 1577456
-  return(year_adjusted_TED / pop_PDX)
+per.capita.TED <- function(sum.11mo) {
+  year.adjusted.TED <- (sum.11mo / 11) + sum.11mo
+  pop.PDX <- 1577456
+  return(year.adjusted.TED / pop.PDX)
 }
 
-TED_summation <- function(tb_teds) {
+TED.summation <- function(tb.teds) {
   VOCa <- 1.4
   VOCb <- 10
   VOCt <- 1
-  tb_teds[, AVOc := pct_auto * VOCa]
-  tb_teds[, AVOb := pct_bus * VOCb]
-  tb_teds[, AVOt := pct_truck * VOCt]
-  tb_teds[, TED  := gp_TED_seg * (AVOc + AVOb + AVOt)]
-  return(tb_teds)
+  tb.teds[, AVOc := pct.auto * VOCa]
+  tb.teds[, AVOb := pct.bus * VOCb]
+  tb.teds[, AVOt := pct.truck * VOCt]
+  tb.teds[, TED  := gp.TED.seg * (AVOc + AVOb + AVOt)]
+  return(tb.teds)
 }
 
-total_excessive_delay <- function(tb_ted) {
-  tb_ted[, TED_seg := ED * PK_HR]
-  tb_ted.a <- tb_ted[, j = list(gp_TED_seg =  sum(TED_seg, na.rm = TRUE),
-                                pct_auto   =  max(pct_auto),
-                                pct_bus    =  max(pct_bus),
-                                pct_truck  =  max(pct_truck)
+total.excessive.delay <- function(tb.ted) {
+  tb.ted[, TED.seg := ED * PK.HR]
+  tb.ted.a <- tb.ted[, j = list(gp.TED.seg =  sum(TED.seg, na.rm = TRUE),
+                                pct.auto   =  max(pct.auto),
+                                pct.bus    =  max(pct.bus),
+                                pct.truck  =  max(pct.truck)
                               ), by = Tmc]
-  return(tb_ted.a)
+  return(tb.ted.a)
 }
 
-peak_hr <- function(tb_pk) {
-  tb_pk[, PK_HR := dir_aadt * `2015_15-min_Combined`]
-  return(tb_pk)
+peak.hr <- function(tb.pk) {
+  tb.pk[, PK.HR := dir.aadt * `2015_15-min_Combined`]
+  return(tb.pk)
 }
 
-excessive_delay <- function(tb_ed) {
-  tb_ed[, ED := RSD / 3600]
-  tb_ed[ED < 0, ED := 0]
-  return(tb_ed)
+excessive.delay <- function(tb.ed) {
+  tb.ed[, ED := RSD / 3600]
+  tb.ed[ED < 0, ED := 0]
+  return(tb.ed)
 }
 
-RSD <- function(tb_rsd) {
-  tb_rsd[, RSD := travel_time_seconds - SD]
-  tb_rsd[RSD < 0, RSD := 0]
-  return(tb_rsd)
+RSD <- function(tb.rsd) {
+  tb.rsd[, RSD := travel_time_seconds - SD]
+  tb.rsd[RSD < 0, RSD := 0]
+  return(tb.rsd)
 }
 
-segment_delay <- function(tb_sd) {
-  tb_sd[, SD := (miles / TS) * 3600]
-  return(tb_sd)
+segment.delay <- function(tb.sd) {
+  tb.sd[, SD := (miles / TS) * 3600]
+  return(tb.sd)
 }
 
-AADT_splits <- function(tb_spl) {
-  tb_spl[, dir_aadt := aadt / faciltype]
-  tb_spl[, aadt_auto := dir_aadt - (aadt_singl + aadt_combi)]
-  tb_spl[, pct_auto := aadt_auto / dir_aadt]
-  tb_spl[, pct_bus := aadt_singl / dir_aadt]
-  tb_spl[, pct_truck := aadt_combi / dir_aadt]
-  return(tb_spl)
+AADT.splits <- function(tb.spl) {
+  tb.spl[, dir.aadt := aadt / faciltype]
+  tb.spl[, aadt.auto := dir.aadt - (aadt_singl + aadt_combi)]
+  tb.spl[, pct.auto := aadt.auto / dir.aadt]
+  tb.spl[, pct.bus := aadt_singl / dir.aadt]
+  tb.spl[, pct.truck := aadt_combi / dir.aadt]
+  return(tb.spl)
 }
 
-threshold_speed <- function(tb_ts) {
-  tb_ts[, TS := SPEED_LIMIT * .6]
-  tb_ts[TS < 20 | is.na(TS), TS := 20]
-  return(tb_ts)
+threshold.speed <- function(tb.ts) {
+  tb.ts[, TS := SPEED_LIMIT * .6]
+  tb.ts[TS < 20 | is.na(TS), TS := 20]
+  return(tb.ts)
 }
 
 ###############################################################################
@@ -83,52 +83,52 @@ tb <- data.table(
 
 setwd("H:/map21/perfMeasures/phed/data")
 
-drive_path <- "original_data/"
+drive.path <- "original_data/"
 quarters <- c("2017Q1", "2017Q2", "2017Q3", "2017Q4")
-folder_end <- "_TriCounty_Metro_15-min"
-file_end <- "_NPMRDS (Trucks and passenger vehicles).csv"
+folder.end <- "_TriCounty_Metro_15-min"
+file.end <- "_NPMRDS (Trucks and passenger vehicles).csv"
 
 for (q in quarters) {
-  filename <- paste(q, folder_end, file_end, sep = "")
-  path <- paste(q, folder_end, sep = "")
-  full_path <- paste(path, filename, sep = "/")
-  tb_temp <- fread(paste(drive_path, full_path, sep = ""))
-  tb <- rbind(tb, tb_temp)
+  filename <- paste(q, folder.end, file.end, sep = "")
+  path <- paste(q, folder.end, sep = "")
+  full.path <- paste(path, filename, sep = "/")
+  tb.temp <- fread(paste(drive.path, full.path, sep = ""))
+  tb <- rbind(tb, tb.temp)
 }
 
 #tb <- fread("C:/Users/saavedrak/metro_work/PHED/Feb2017_test/Feb2017_test.csv")
 # Filter out weekends:
 tb <- tb[!(weekdays(ymd_hms(measurement_tstamp)) %in% c("Saturday", "Sunday"))]
 # Extract hour from datetime timestamp:
-tb[, pk_hr := hour(measurement_tstamp)]
-tb_peak <- fread("peakingFactors_join_edit.csv")
+tb[, pk.hr := hour(measurement_tstamp)]
+tb.peak <- fread("peakingFactors_join_edit.csv")
 # Convert text time to standalone integer hour. 
-tb_peak[, pk_hour := as.integer(hour(parse_date_time(startTime, orders="HM")))]
+tb.peak[, pk.hour := as.integer(hour(parse_date_time(startTime, orders="HM")))]
 # Left join to main data file.
-tb <- merge(x = tb, y = tb_peak, by.x = "pk_hr", by.y = "pk_hour",
+tb <- merge(x = tb, y = tb.peak, by.x = "pk.hr", by.y = "pk.hour",
             all.x = TRUE)
-tb <- tb[pk_hr %in% c(6, 7, 8, 9, 10, 15, 16, 17, 18, 19)]
+tb <- tb[pk.hr %in% c(6, 7, 8, 9, 10, 15, 16, 17, 18, 19)]
 # Inner join with urban areas only
-tb_urban <- fread("urban_tmc.csv")
-tb <- merge(x = tb_urban, y = tb, by.x = "Tmc", by.y = "tmc_code")
+tb.urban <- fread("urban_tmc.csv")
+tb <- merge(x = tb.urban, y = tb, by.x = "Tmc", by.y = "tmc_code")
 # Merge metadata
-tb_meta <- fread(
+tb.meta <- fread(
   "TMC_Identification_NPMRDS (Trucks and passenger vehicles).csv")
-tb <- merge(x = tb, y = tb_meta, by.x = "Tmc", by.y = "tmc")
+tb <- merge(x = tb, y = tb.meta, by.x = "Tmc", by.y = "tmc")
 # Join HERE data
-tb_here <- fread("HERE_OR_Static_TriCounty_edit.csv")
-tb <- merge(x = tb, y = tb_here, by.x = "Tmc", by.y = "TMC_HERE", all.x = TRUE)
+tb.here <- fread("HERE_OR_Static_TriCounty_edit.csv")
+tb <- merge(x = tb, y = tb.here, by.x = "Tmc", by.y = "TMC_HERE", all.x = TRUE)
 
-tb <- threshold_speed(tb)
-tb <- AADT_splits(tb)
-tb <- segment_delay(tb)
+tb <- threshold.speed(tb)
+tb <- AADT.splits(tb)
+tb <- segment.delay(tb)
 tb <- RSD(tb)
-tb <- excessive_delay(tb)
-tb <- peak_hr(tb)
-tb <- total_excessive_delay(tb)
-tb <- TED_summation(tb)
+tb <- excessive.delay(tb)
+tb <- peak.hr(tb)
+tb <- total.excessive.delay(tb)
+tb <- TED.summation(tb)
 
-result <- per_capita_TED(sum(tb6[, TED]))
+result <- per.capita.TED(sum(tb[, TED]))
 print(result)
 
 end.time <- Sys.time()
